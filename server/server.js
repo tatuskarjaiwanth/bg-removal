b986786813c4628eca560fa09d989cc807a64920
TJaiwanth3 <jaiwanthtatuskar2@gmail.com>
2025-07-12 15:18:54 +0530

    Implement Clerk webhooks handling and set up user routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 //App Config
 const PORT = process.env.PORT || 4000;
@@ -17,6 +18,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
