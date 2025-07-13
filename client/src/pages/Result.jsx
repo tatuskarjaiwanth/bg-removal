1fb80247d3f9e4762f14fbe7101bba047bfd0104
TJaiwanth3 <jaiwanthtatuskar2@gmail.com>
2025-07-13 14:00:41 +0530

    Refactor Result component: Uncomment navigation logic and clean up code

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
 const Result = () => {
-  const {resultImage , image} = useContext(AppContext)
+  const {resultImage , image /*, setImage, setResultImage*/} = useContext(AppContext)
+//   const navigate = useNavigate()
+//   const handleTryAnother = () => {
+//   setImage(false)
+//   setResultImage(false)
+//   navigate('/')
+// }
   return (
     <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
       <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm'>
@@ -13,6 +20,7 @@ const Result = () => {
           {/*Left side*/}
           <div>
             <p className='font-semibold text-gray-600 mb-2'>Original</p>
+            
             <img className='rounded-md border' src={image ? URL.createObjectURL(image): ''} alt=""/>
           </div>
           {/*---right side---- */}
@@ -33,7 +41,7 @@ const Result = () => {
         </div>
         {/* Buttons */}
        { resultImage && <div className='flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6'>
-          <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700  '>Try another image</button>
+          <button  className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700  '>Try another image</button>
           <a  href={resultImage} download className='px-8 py-2.5 text-white text-sm bg-gradient-to-r from-violet-600 to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700 '>Download Image</a>
         </div>
         }
